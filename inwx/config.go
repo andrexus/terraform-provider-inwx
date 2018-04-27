a6f1f00e4ab1118deb2112f1b8bde7dcf60e2e29
Andrew <andrexus@gmail.com>
2018-04-27 15:00:26 +0200

    add more log statements

--- inwx/config.go
+++ inwx/config.go
@@ -2,9 +2,9 @@ package inwx
 
 import (
 	"log"
+	"strings"
 
 	"github.com/andrexus/goinwx"
-	"strings"
 )
 
 type Config struct {
@@ -17,13 +17,15 @@ type Config struct {
 func (c *Config) Client() (*goinwx.Client, error) {
 	clientOpts := &goinwx.ClientOptions{Sandbox: c.Sandbox}
 	client := goinwx.NewClient(c.Username, c.Password, clientOpts)
+	log.Printf("[INFO] Trying to login with provided credentials")
 	err := client.Account.Login()
 	if err != nil {
 		return nil, err
 	}
+	log.Printf("[INFO] Login successful")
 	if c.TAN != "" {
-		unlockErr := client.Account.Unlock(strings.Replace(c.TAN, " ", "", -1))
-		if err != nil {
+		log.Printf("[INFO] 2-factor-auth is configured. Trying to unlock account")
+		if unlockErr := client.Account.Unlock(strings.Replace(c.TAN, " ", "", -1)); unlockErr != nil {
 			return nil, unlockErr
 		}
 	}
